b60a12ac5a281cedf5301de2e5480bfdebd2c7be
Erwan_hz_ <erwan.houze@viacesi.fr>
2025-04-09 09:53:35 +0200

    changements header (visuel + ajout logo)

--- apps/frontend/commercial-app/src/components/Header.js
+++ apps/frontend/commercial-app/src/components/Header.js
@@ -1,14 +1,16 @@
 "use client";
 
 import Link from 'next/link';
+import Image from 'next/image';
 
 const Header = () => {
   return (
     <header className="bg-card-bg shadow-sm fixed top-0 left-0 right-0 z-10 border-b border-border-color">
-      <div className="container mx-auto px-4 py-3 flex justify-between items-center">
+      <div className="flex justify-between items-center py-3 pl-2">
         <div className="flex items-center">
-          <Link href="/">
-            <h1 className="text-xl font-bold text-primary">CES'EATS</h1>
+          <Link href="/" className="flex items-center">
+            <Image src="/logo-svg.svg" alt="CES'EATS Logo" width={50} height={40} />
+            <h1 className="text-xl font-bold text-black">CES'EATS</h1>
           </Link>
           <span className="ml-4 text-text-secondary hidden md:inline font-medium">Administration Commerciale</span>
         </div>
